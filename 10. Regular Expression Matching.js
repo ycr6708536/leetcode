555a138530ec6606d3aede4e93423287473f7135
闫超仁 <yanchaoren@xiaomi.com>
2017-11-21 21:25:36 +0800

    10.regular-expression-matching

--- 10. Regular Expression Matching.js
+++ 10. Regular Expression Matching.js
@@ -1,63 +1,63 @@
-/*
-Implement regular expression matching with support for '.' and '*'.
-
-https://leetcode.com/problems/regular-expression-matching/
-*/
-
-
+/**
+ * Implement regular expression matching with support for '.' and '*'.
+ * 
+ * https://leetcode.com/problems/regular-expression-matching/description/
+ * 
+ */
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
 var isMatch = function(s, p) {
-  let sl = s.length,
-    pl = p.length,
-    i = 0,
-    j = 0;
-let tpl =p.replace(/\*/g,'').length;
-if(tpl>sl+pl-tpl)return false;  
-  if (p[0] === '*') return false;
-
-  while (i < sl && j < pl) {
-    if (p[j] === '.') {
-      if (p[j + 1] === '*') {
-        j += 2;
-        if (j < pl) {
-          let tem = s.substr(i);
-          let index = tem.indexOf(p[j]);
-          if (index < 0) {
-            i = 0;
-            break;
-          } else {
-            i = i + index + 1;
-            j += 1;
-          }
-        } else {
-          i = sl;
+  if (!s || !p) { //参数为空
+    // 正则为空
+    if (p != '' && !s) {
+      var dp = [true];
+      for (var i = 0; i <= p.length; i++) {
+        if (p[i] == '*' && dp[i - 1]) {
+          dp[i + 1] = true;
         }
-
-      } else {
-        i++;
-        j++;
       }
-    } else if (s[i] === p[j]) {
-      i++;
-      j++;
-    } else if (p[j] === '*') {
-      if (p[j - 1] !== s[i] && p[j - 1] !== '.') {
-        j++;
-      } else {
-        i++;
-      }
+      return !!dp[p.length]
+    } else if (!s && !p) {
+      // 都为空
+      return true;
     } else {
-      if(p[++j]==='*'){
-        j++;
-        i++;
-      }else{
-        i = 0;
-      }
+      return false;
     }
   }
 
-  return i === sl
+  var dp = [];
+  for (var i = 0; i <= s.length; i++) {
+    dp[i] = [];
+    for (var j = 0; j <= p.length; j++) {
+      dp[i][j] = false;
+    }
+  }
+
+  dp[0][0] = true;
+  for (i = 0; i < p.length; i++) {
+    if (p[i] == '*' && dp[0][i - 1]) {
+      dp[0][i + 1] = true;
+    }
+  }
+
+  for (i = 0; i < s.length; i++) {
+    for (var j = 0; j < p.length; j++) {
+      if (p[j] == '.' || p[j] == s[i]) {
+        dp[i + 1][j + 1] = dp[i][j];
+      }
+      if (p[j] == '*') {
+        if (p[j - 1] != s[i] && p[j - 1] != '.') {
+          dp[i + 1][j + 1] = dp[i + 1][j - 1];
+        } else {
+          dp[i + 1][j + 1] = dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1];
+        }
+      }
+    }
+  }
+  return dp[s.length][p.length];
 };
 
-// 
-// IsMatch("aaa","ab*a") false WHY ???
-console.log(isMatch('aaa', 'ab*ac*a'))
+console.log(isMatch('', '.*'));
