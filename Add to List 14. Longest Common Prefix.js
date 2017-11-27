cbb68209244f350b842dcb69bb7fc9478b3f35c8
闫超仁 <24732077@qq.com>
2017-11-27 17:18:02 +0800

    14. Longest Common Prefix

---
+++ Add to List 14. Longest Common Prefix.js
@@ -0,0 +1,36 @@
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+
+
+ * 
+ * https://leetcode.com/problems/longest-common-prefix/description/
+ * 
+ * 
+ */
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  let r = '';
+  if (strs.length <= 0) return r;
+  let l = strs.length;
+
+  for (let i = 0; i < strs[0].length; i++) {
+    let tr = r + strs[0][i];
+    let reg = new RegExp('^' + tr);
+    for (var j = 0; j < l; j++) {
+      if (!reg.test(strs[j])) {
+        break;
+      }
+    }
+    if (j == l) {
+      r = tr;
+    } else {
+      break;
+    }
+  }
+
+  return r;
+};
