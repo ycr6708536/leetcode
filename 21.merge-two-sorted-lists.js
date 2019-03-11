b806ddd9354341bce860f3a5798dc21f20036426
yanchaoren <yanchaoren@didichuxing.com>
2019-03-11 18:08:58 +0800

    feat(21.merge-two-sorted-lists.js):

---
+++ 21.merge-two-sorted-lists.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=21 lang=javascript
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (45.86%)
+ * Total Accepted:    515.9K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * Merge two sorted linked lists and return it as a new list. The new list
+ * should be made by splicing together the nodes of the first two lists.
+ *
+ * Example:
+ *
+ * Input: 1->2->4, 1->3->4
+ * Output: 1->1->2->3->4->4
+ *
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+function ListNode(val) {
+    this.val = val
+    this.next = null
+}
+
+var mergeTwoLists = function(l1, l2) {
+    var r
+    var rr
+    if (!l1) return l2
+    if (!l2) return l1
+    if (l1.val < l2.val) {
+        rr = new ListNode(l1.val)
+        l1 = l1.next
+    } else {
+        rr = new ListNode(l2.val)
+        l2 = l2.next
+    }
+    r = rr
+
+    while (l1 || l2) {
+        if (!l1 && l2) {
+            r.next = l2
+            break
+        } else if (l1 && !l2) {
+            r.next = l1
+            break
+        } else if (l1.val < l2.val) {
+            r.next = new ListNode(l1.val)
+            l1 = l1.next
+        } else {
+            r.next = new ListNode(l2.val)
+            l2 = l2.next
+        }
+        r = r.next
+    }
+    return rr
+}
