4d90d3cb27d3e079409caf35d8ba65cc06422297
闫超仁 <yanchaoren@xiaomi.com>
2019-03-17 15:16:17 +0800

    KMP

--- 28.implement-str-str.js
+++ 28.implement-str-str.js
@@ -69,24 +69,43 @@
 //     return -1
 // };
 
+function getNext(str) {
+    var len = str.length;
+    var next = [-1];
+    var j = 0,
+        k = -1;
+
+    while (j < len - 1) {
+        if (k === -1 || str[j] === str[k]) {
+            next[++j] = ++k
+        } else {
+            k = next[k]
+        }
+    }
+    return next
+}
+
+
 var strStr = function (haystack, needle) {
     var l1 = haystack.length;
     var l2 = needle.length;
-    var i1=i2=0;
-    if(l2===0)return 0
-    if(l1===0)return -1
-    if(l1<l2)return -1
+    var next = getNext(needle);
+    var i1 = i2 = 0;
+    if (l2 === 0) return 0
+    if (l1 === 0) return -1
+    if (l1 < l2) return -1
 
-    while(i1<l1){
-        if(haystack[i1+i2]===needle[i2]){
-            if(i2 === l2-1){
-                return i1;
-            }else{
-                i2++;
+    while (i1 < l1) {
+        if (i2 === -1 || haystack[i1] === needle[i2]) {
+            if (i2 === l2 - 1) {
+                return i1 - i2
+            } else {
+                i2++
+                i1++
             }
-        }else{
-           i1 +=i2>1?i2-1:1;
-           i2=0;
+        } else {
+            i2 = next[i2]
+
         }
     }
     return -1
