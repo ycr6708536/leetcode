4d5681bebbe742ed724a44ef9d05685b4ce293a6
yanchaoren <yanchaoren@didichuxing.com>
2019-03-22 19:25:05 +0800

    feat(添加内容):

--- 58.length-of-last-word.js
+++ 58.length-of-last-word.js
@@ -26,23 +26,36 @@
  * 
  * 
  */
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+// var lengthOfLastWord = function (s) {
+//     // 空字符串 末尾是 ‘ ’ 的最后一个单词长度是0
+//     var last = 0;
+//     var cur = 0;
+//     for (var i = 0; i < s.length; i++) {
+//         if (s[i] !== ' ') {
+//             cur++;
+//         } else {
+//             last = cur > 0 ? cur : last;
+//             cur = 0;
+//         }
+//     }
+//     last = cur > 0 ? cur : last;
+//     return last;
+
+// };
 /**
  * @param {string} s
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
     // 空字符串 末尾是 ‘ ’ 的最后一个单词长度是0
-    var last = 0;
-    var cur = 0;
-    for (var i = 0; i < s.length; i++) {
-        if (s[i] !== ' ') {
-            cur++;
-        } else {
-            last = cur > 0 ? cur : last;
-            cur = 0;
-        }
+    s = s.trim()
+    var count=0;
+    for (var i = s.length-1; i >-1; i--,count++) {
+        if(s[i]===' ')return count
     }
-    last = cur > 0 ? cur : last;
-    return last;
-
+    return count;
 };
